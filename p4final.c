4b998489aff978f02c5c49d9371eb102abc553a0
sanjanaraghupat <96220467+sanjanaraghupathi@users.noreply.github.com>
2022-02-24 14:22:34 +0000

    fibonacci sequence

--- p4final.c
+++ p4final.c
@@ -1,1 +1,32 @@
-
+#include <stdio.h>
+int input()
+{
+  int n;
+  printf("enter the value up to which the series is required\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{
+  int a=0,b=1,sum;
+  for(int i=1;i<n;i++)
+    {
+      sum=a+b;
+      a=b;
+      b=sum;
+    }
+  return a;
+}
+void output(int n, int fibo)
+{
+ printf("\n"); 
+   printf(" %d th term of fibonacci sequence is %d \n",n,fibo); 
+}
+int main()
+{
+  int n,fibo;
+  n=input();
+  fibo=find_fibo(n);
+  output(n,fibo);
+  return 0;
+}
--- p5final.c
+++ p5final.c
@@ -1,1 +1,49 @@
-
+#include<stdio.h>
+#include<math.h>
+int input_array_size()
+{
+  int a;
+  printf("enter the array size");
+  scanf("%d",&a);
+  return a;
+}
+void init_array(int n, int a[n])
+{
+  for(int i=0;i<n;i++)
+    a[i]=i;
+    a[0]=0;
+   a[1]=0;
+}
+void erotosthenes_sieve(int n, int a[n])
+{
+  int i=0;
+  while(i<sqrt(n))
+    {
+      while(a[i]==0)
+        i++;
+      for(int k=i+i;i<n;k+=i)
+        {
+        a[k]=0;
+      i++;
+          }
+    }
+}
+void out_put(int n, int a[n])
+{
+  for(int i=0;i<n;i++)
+    {
+      if(a[i]!=0)
+        printf("%d",a[i]);
+    }
+  printf("/n");
+  }
+int main()
+{
+  int n,a[n];
+  n=input_array_size();
+  init_array(n,a);
+  erotosthenes_sieve(n,a);
+  out_put( n,a);
+  return 0;
+    
+}
