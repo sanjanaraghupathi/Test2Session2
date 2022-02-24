cdf9e99705bc8bbae95df5a06cb5673c5e4e3c39
sanjanaraghupat <96220467+sanjanaraghupathi@users.noreply.github.com>
2022-02-24 09:16:18 +0000

    done 1 2 3

--- p2final.c
+++ p2final.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include<math.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+  printf("enter first points");
+  scanf("%f%f",x1,y1);
+  printf("enter second points");
+  scanf("%f%f",x2,y2);
+  printf("enter third points");
+  scanf("%f%f",x3,y3);
+  
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  int area;
+  area=0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+  if(area==0)
+  {
+    return 1;
+  }
+  
+  else
+  {
+    return 2;
+  }
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
+{
+  if(istriangle==1)
+  {
+    printf("three points  do not form a triangle\n");
+  }
+  if(istriangle==2)
+  {
+    printf("three points form a triangle\n");
+  }
+   
+}
+int main()
+{
+  float x1,y1,x2,y2,x3,y3;
+  float area;
+  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+ area= is_triangle( x1,y1,x2,y2,x3,y3);
+  output(x1,y1,x2,y2,x3,y3,area);
+  return 0;
+}
 
--- p3final.c
+++ p3final.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+int input_number()
+{
+  int x;
+  printf("enter a number\n");
+  scanf("%d",&x);
+  return x;
+}
+int is_prime(int n)
+{
+  int count=0;
+  for(int i=1;i<=n;i++)
+    {
+      if(n%i==0)
+      
+        count=count+1;
+      }
+      if(count>2)
+      {
+        return 1;
+      }
+      else
+      {
+        return 2;
+      }
+    }
 
+void output(int n,int prime)
+{
+  if (prime==1)
+  {
+    printf("%d is not  a prime number\n",n);
+  }
+  else
+   {
+    printf("%d is  a prime number\n",n);
+  }  
+}
+int main()
+{
+  int n,prime;
+  n=input_number();
+  prime=is_prime(n);
+  output(n,prime);
+  return 0;
+}
